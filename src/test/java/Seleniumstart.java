3ebc228ffdbd2fce23c0b0582e97c641b887d341
Avarion <bogoborec87@gmail.com>
2023-02-21 21:00:58 +0200

    add

--- src/test/java/Seleniumstart.java
+++ src/test/java/Seleniumstart.java
@@ -1,40 +1,41 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Seleniumstart {
 
     WebDriver wd;
-    String str;
-    int i;
-
-    public void nonStatic(){
-        System.out.println(this.i);
-    }
-    public static void staticMethod(){
-        System.out.println(7-9);
-    }
 
     @BeforeMethod
-    public void preCondition(){
+    public void preCondition() {
         System.out.println("Opening web-site");
         wd = new ChromeDriver();
-
-
+//      wd.get("https://telranedu.web.app/home");
+        wd.navigate().to("https://telranedu.web.app/home");
+//      wd.navigate().forward();
+//      wd.navigate().back();
     }
 
 
     @Test
-    public void testLogin(){
-
+    public void testLogin() {
+//        System.out.println("Test is over");
+        WebElement element = wd.findElement(By.tagName("a"));
+        List<WebElement> elements = wd.findElements(By.tagName("a"));
+        System.out.println(elements.size());
     }
 
 
     @AfterMethod
-    public void postCondition(){
-
+    public void postCondition() {
+//        wd.close();
+        wd.quit();
     }
 
 
